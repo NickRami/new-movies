f34e60942e20dd063ac809c69745b18a58ff5846
Ramiro Silva <ramisilva8@gmail.com>
2026-02-25 18:07:39 -0300

    build: Implement vendor chunking and adjust chunk size warning limit in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,18 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
 
 
